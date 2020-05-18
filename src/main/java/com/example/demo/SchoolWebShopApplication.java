6a109831fb64b786052a3c940082d400f9870f6e
Unibeam <beyondevery@live.com>
2020-05-18 15:44:39 +0800

    demo

---
+++ src/main/java/com/example/demo/SchoolWebShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolWebShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolWebShopApplication.class, args);
+	}
+
+}
